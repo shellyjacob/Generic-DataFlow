f33ef8b36221eb54c47dc01ec885c304a532a27f
shellyjacob <sh3llyjacob@gmail.com>
2025-06-28 16:19:53 +0300

    question 6a - added class UpperCaseBlock that implements interface Iblock with string.

---
+++ Generics/Generics/UpperCaseBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class UpperCaseBlock : IBlock<string>
+    {
+        public string Process(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
