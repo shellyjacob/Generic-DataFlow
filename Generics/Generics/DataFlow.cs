753886f4fca0aeb5944a2382953d7158e41a5ac1
shellyjacob <sh3llyjacob@gmail.com>
2025-06-28 15:26:05 +0300

    question 3 (or 5) - Implenmented the interface IDataFlow<T> - created class DataFlow.

---
+++ Generics/Generics/DataFlow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class DataFlow<T> : IDataFlow<T>  where T : class
+    {
+        private List<IBlock<T>> _blocksList = new List<IBlock<T>>();
+
+        public T RunFlow(T input)
+        {
+            foreach (IBlock<T> block in _blocksList)
+            {
+                input = block.Process(input);
+            }
+
+            return input;
+        }
+
+        public void AddBlock(IBlock<T> block)
+        {
+            _blocksList.Add(block);
+        }
+    }
+}
